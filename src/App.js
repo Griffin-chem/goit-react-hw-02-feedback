d8f45292a1f9d48de39fd147140d4eb7539b2b7f
Griffin <chemist_khnu@rambler.ru>
2020-09-06 18:58:38 +0300

    Step 2

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ class App extends Component {
     bad: 0
   };
 
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, elem) => acc + elem, 0)
+  };
+
+  countPositiveFeedbackPercentage = () => {
+   return (this.state.good === 0) ? 0 : Math.round(this.state.good / this.countTotalFeedback() * 100)
+  }
+
   handleClick = (e) => {
     const newState = {[e.target.name]: this.state[e.target.name] + 1}
     this.setState(newState);
@@ -18,12 +26,20 @@ class App extends Component {
   render () { 
     return (
       <div>
-        <button name="good" onClick={this.handleClick}>Good</button>
-        <button name="neutral" onClick={this.handleClick}>Neutral</button>
-        <button name="bad" onClick={this.handleClick}>Bad</button>
-        <p>Good: {this.state.good}</p>
-        <p>Neutral: {this.state.neutral}</p>
-        <p>Bad: {this.state.bad}</p>
+        <section>
+          <p>Please, leave feedback</p>
+          <button name="good" onClick={this.handleClick}>Good</button>
+          <button name="neutral" onClick={this.handleClick}>Neutral</button>
+          <button name="bad" onClick={this.handleClick}>Bad</button>
+        </section>
+        <section>
+          <p>Statistic</p>
+          <p>Good: {this.state.good}</p>
+          <p>Neutral: {this.state.neutral}</p>
+          <p>Bad: {this.state.bad}</p>
+          <p>Total: {this.countTotalFeedback()}</p>
+          <p>Positive Feedback: {this.countPositiveFeedbackPercentage()}%</p>
+        </section>
       </div>
     );
   }
